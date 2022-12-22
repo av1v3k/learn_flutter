f74dc379bd6361565542dbc6b73c8d7ff3178e00
vanan07 <Vivek.Ananth@albertsons.com>
2022-12-23 05:15:04 +0530

    voidcallback to Function; answerFunction with parameter change;

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -21,21 +21,40 @@ class _MyAppState extends State<MyApp> {
   List questions = const [
     {
       "questionText": "What is your favourite Color?",
-      "answer": ["Red", "Blue", "Green", "Yellow"]
+      "answer": [
+        {"text": "Red", "score": 5},
+        {"text": "Blue", "score": 10},
+        {"text": "Green", "score": 10},
+        {"text": "Yellow", "score": 4}
+      ]
     },
     {
       "questionText": "What is your favourite Sweet?",
-      "answer": ["Jamun", "Candy", "Ice-cream", "Falooda"]
+      "answer": [
+        {"text": "Jamun", "score": 5},
+        {"text": "Candy", "score": 10},
+        {"text": "Falooda", "score": 10},
+        {"text": "Ice-cream", "score": 4}
+      ]
     },
     {
       "questionText": "What is your favourite Hobby?",
-      "answer": ["Running", "Reading", "Music", "Travel"]
+      "answer": [
+        {"text": "Running", "score": 5},
+        {"text": "Music", "score": 10},
+        {"text": "Travelling", "score": 10},
+        {"text": "Trekking", "score": 4}
+      ]
     }
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
   // const double _buttonfontSize = 10;
 
-  void answerFunction() {
+  void _answerQuestion(int score) {
+
+    _totalScore = _totalScore + score;
+    
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -55,7 +74,7 @@ class _MyAppState extends State<MyApp> {
                   style: TextStyle(color: Colors.black)),
               backgroundColor: Colors.amberAccent),
           body: _questionIndex < questions.length
-              ? Quiz(questions, _questionIndex, answerFunction)
+              ? Quiz(questions, _questionIndex, _answerQuestion)
               : const Result()),
     );
   }
--- first_app/lib/quiz.dart
+++ first_app/lib/quiz.dart
@@ -5,7 +5,7 @@ import './answer.dart';
 class Quiz extends StatelessWidget {
   final List questions;
   final int _questionIndex;
-  final VoidCallback answerFunction;
+  final Function answerFunction;
 
   const Quiz(this.questions, this._questionIndex, this.answerFunction,
       {super.key});
@@ -14,8 +14,10 @@ class Quiz extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(mainAxisAlignment: MainAxisAlignment.start, children: [
       Question(questions[_questionIndex]['questionText']),
-      ...(questions[_questionIndex]['answer'] as List<String>).map((answer) {
-        return Answer(answer, answerFunction);
+      ...(questions[_questionIndex]['answer'] as List<Map<String, Object>>)
+          .map((answer) {
+        return Answer(
+            answer['text'] as String, () => answerFunction(answer['score']));
       }).toList()
     ]);
   }
