45d09dc14981ce24ad5819640c856d75e64ae1f5
vanan07 <Vivek.Ananth@albertsons.com>
2022-12-23 05:40:37 +0530

    with the score obtained, modify text dynamically using a function/getter;

--- first_app/lib/result.dart
+++ first_app/lib/result.dart
@@ -3,12 +3,28 @@ import 'package:flutter/material.dart';
 class Result extends StatelessWidget {
   final int resultScore;
 
+  String get _resultPhrase {
+    String resultText = 'Awesome !';
+    if (resultScore <= 8) {
+      resultText = 'Not Bad !';
+    } else if (resultScore <= 12) {
+      resultText = 'OMG!';
+    } else if (resultScore <= 20) {
+      resultText = 'Smashed it!';
+    }
+    return resultText;
+  }
+
   const Result(this.resultScore, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Thank you for playing', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+    return Center(
+      child: Text(
+        _resultPhrase,
+        style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
